17680de69c2009bd290ee2c9570071f8716d374e
Enu Shalisha <enushailsha352@gmail.com>
2021-01-21 13:49:55 +0900

    test

---
+++ src/com/ex0121/Ex01wh.java
@@ -0,0 +1,24 @@
+package com.ex0121;
+
+public class Ex01wh {
+
+	public static void main(String[] args) {
+		int s, n;
+		/*s=0; n=0;
+		while(n<100) {
+			n+=2;
+			s+=n;
+			System.out.println(n);
+		}
+		System.out.println("결과: "+s);*/
+		
+		s=0; n=1;
+		while(n<100) {
+			s+=n;
+			n+=2;
+			System.out.println(n);
+		}
+		System.out.println("결과: "+s);
+	}
+
+}
---
+++ src/com/ex0121/Ex02wh.java
@@ -0,0 +1,29 @@
+package com.ex0121;
+
+import java.util.Scanner;
+
+public class Ex02wh {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		System.out.println("숫자 입력:");
+		a=sc.nextInt();
+		int n=1;
+		int s=0;
+		if(a<0)
+			{System.out.println("입력 오류입니다.");}
+		else{
+			while(n<=a) {
+			s+=n;
+			n+=2;
+			System.out.println(n);
+			}
+			System.out.printf("1부터 %d까지의 합은 %d입니다.", a, s);
+		}
+		//test2
+		sc.close();
+
+	}
+
+}
