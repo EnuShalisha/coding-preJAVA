6a36bd6087639a8ddb931ab363cb951b84a7b1b4
Enu Shalisha <enushailsha352@gmail.com>
2021-01-21 18:10:17 +0900

    HW_0121_0.4

--- src/hw_0121/AD07.java
+++ src/hw_0121/AD07.java
@@ -10,27 +10,27 @@ public class AD07 {
 		do	{System.out.println("수? ");
 			a=sc.nextInt();}
 		while(a<0);
-		int count=1; int b, c;
-		b=0; c=1;
+		
+		int count=1; int b=0, c=0;
 		while(a-c>=0){
 			c=c*10;
 			b++;
 		}
 		c/=10;
+		
 		int temp=0, result=0;
 		while(b>0) {
 			temp=a;
 			a/=c;
-			System.out.println(a);
 			result+=count*a;
 			a=temp-a*c;
-			System.out.println(result);
 			c/=10;
-			//System.out.println(c);
 			count*=10;
 			b--;
 		}
+		
 		System.out.println(result);
+		sc.close();
 		
 	}
 
