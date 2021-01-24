d7d5eecfe306d822903d6dc2b4e5ec2d325bb973
Enu Shalisha <enushailsha352@gmail.com>
2021-01-24 12:16:18 +0900

    temporary

---
+++ src/com/quiz0121/Ex007_Hap.java
@@ -0,0 +1,13 @@
+package com.quiz0121;
+public class Ex007_Hap {
+	public static void main(String[] args) {
+/*
+		1~10 까지 합을 계산하여 출력하는 프로그램
+*/
+		int a=0, s=0;
+		while(a++ <= 9) {
+		    s+=a;
+		}
+		System.out.println("a:" + a + ", s:" + s);
+	}
+}
---
+++ src/com/quiz0121/Ex101_dowhile.java
@@ -0,0 +1,17 @@
+package com.quiz0121;
+public class Ex101_dowhile {
+	public static void main(String[] args) {
+/*
+		1~10 까지 수를 출력하는 프로그램
+*/
+		int n;
+		
+		n=0;
+		do {
+			n++;
+			System.out.print(n+"  ");
+		}while(n<10);
+		System.out.println();
+		// System.out.println(n); // 10
+	}
+}
