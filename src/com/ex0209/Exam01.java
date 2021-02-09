7f748fb647f95cc6cfe9d2580ea07a563ad63e54
user <user@DESKTOP-VQJJ64C>
2021-02-09 12:23:44 +0900

    0209_0.3

--- src/com/ex0209/Exam01.java
+++ src/com/ex0209/Exam01.java
@@ -1,5 +1,6 @@
 package com.ex0209;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,6 +10,7 @@ public class Exam01 {
 		Scanner sc = new Scanner(System.in);
 		Extest1 t = new Extest1();
 		
+		while(true) {
 		try {
 			System.out.println("연산할 두 수를 입력하십시오.");
 			System.out.println("첫번째: ");
@@ -19,19 +21,31 @@ public class Exam01 {
 			char c=sc.next().charAt(0);
 			t.operation(a, b, c);
 			System.out.println("연산 완료");
-		} catch (InputMismatchException e) {
-			System.out.println("문자를 가지고 연산할 수 없습니다.");}
+			break;
+		}
+		catch (IOException e) {
+			System.out.println("입력값이 없습니다.");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("정수만 입력할 수 있습니다.");
+			sc.nextLine();}
+		catch (OperException e) {
+			System.out.println(e);
+		}
+		catch (ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없습니다.");
+		} 
 		catch (Exception e) {
 			System.out.println("오류 발생");
 		}
 		
 	}
-
+	}
 }
 
 class OperException extends Exception{
 
-	private static final long serialVersionUID = 1L; // 직렬화 나중에 배움
+	private static final long serialVersionUID = 1L;
 	
 	public OperException(String msg) {
 		super(msg);
@@ -52,9 +66,7 @@ class Extest1 {
 			}
 			
 		} catch (ArithmeticException e) {
-			System.out.println("0으로 나눌수 없습니다.");
-		} catch (OperException e) {
-			System.out.println(e);
-		}
+			throw e;
+		} 
 		}
 	}
