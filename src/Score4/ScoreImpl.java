842e08d39ba8681b860259e604b0bcf6c5a1e7fe
Enu Shalisha <enushailsha352@gmail.com>
2021-02-12 15:27:25 +0900

    0212_merge_0.1

--- src/Score4/ScoreImpl.java
+++ src/Score4/ScoreImpl.java
@@ -1,37 +1,91 @@
-package Score4;
+package score4;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ChoiceFormat;
 
-import score2.ScoreVO;
-
-public class ScoreImpl {
-	private List<ScoreVO> list=new ArrayList<>();
+public class ScoreImpl implements Score {
+	private ScoreVO[] list;
+	private int count;
 	
+	public ScoreImpl() {
+		allocation(10); // 초기에 10개의 배열선언
+	}
+	
+	private void allocation(int capacity) {
+		ScoreVO[] temp = new ScoreVO[capacity];
+		if(list!=null && list.length>0) {
+		// if(list.length>0 && list!=null) { // 불가능
+			System.arraycopy(list, 0, temp, 0, list.length);
+		}
+		list=temp;
+	}
+	
+	@Override
+	public int getCount() {
+		return count;
+	}
+	
+	@Override
+	public ScoreVO[] listScore() {
+		return list;
+	}
+	
+	@Override
+	public int append(ScoreVO vo) {
+		if(count>=list.length) {
+			// 배열의 꽉찬 경우 배열을 10개 늘리기
+			allocation(list.length+10);
+		}
+		list[count++]=vo;
+		
+		return count;
+	}
+	
+	@Override
+	public double grade(int score) {
+		double[] limit = {0,60,65,70,75,80,85,90,95};
+		String[] ss= {"0","1.0","1.5","2.0","2.5","3.0","3.5","4.0","4.5"};
+
+		ChoiceFormat cf=new ChoiceFormat(limit, ss);
+		
+		return Double.parseDouble(cf.format(score));
+	}
+	
+	@Override
 	public ScoreVO readScore(String hak) {
 		ScoreVO vo=null;
-		for(ScoreVO vv: list) {
-			if(vv.getHak().equals(hak)) {
-				vo=vv;
+		
+		for(int i=0; i<count; i++) {
+			if(list[i].getHak().equals(hak)) {
+				vo=list[i];
 				break;
 			}
 		}
+		
 		return vo;
 	}
 	
-	public boolean insertScore(ScoreVO vo) {
-		if(readScore(vo.getHak())!=null)
-			return false;
-		list.add(vo);
-		return true;
+	@Override
+	public int indexOf(String hak) {
+		for(int i=0; i<count; i++) {
+			if(list[i].getHak().equals(hak)) {
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 	
-	public boolean deleteScore(String hak) {
-		ScoreVO vo = readScore(hak);
-		if(vo==null)
+	@Override
+	public boolean remove(String hak) {
+		int idx=indexOf(hak);
+		if(idx==-1)
 			return false;
-		list.remove(vo);
+		
+		for(int i=idx; i<count-1; i++) {
+			list[i]=list[i+1];
+		}
+		list[--count]=null;
+		
 		return true;
 	}
 }
-
--- src/Score4/ScoreUI.java
+++ src/Score4/ScoreUI.java
@@ -1,163 +1,232 @@
-package Score4;
+package score4;
 
 import java.util.InputMismatchException;
-import java.util.List;
 import java.util.Scanner;
 
-import score2.ScoreVO;
-
 public class ScoreUI {
-	private Scanner sc = new Scanner(System.in);
-	private ScoreImpl ss =new ScoreImpl();
+	private Scanner sc=new Scanner(System.in);
+	private Score ss = new ScoreImpl();
 	
 	public void menu() {
 		int ch;
-		
 		while(true) {
 			try {
 				do {
-					System.out.println("1. ë“±ë¡ 2. ìˆ˜ì • 3. ì‚­ì œ 4. í•™ë²ˆê²€ìƒ‰ 5. ì´ë¦„ê²€ìƒ‰ 6. ë¦¬ìŠ¤íŠ¸ 7. ì¢…ë£Œ => ");
+					System.out.println("1.ÀÔ·Â 2.¼öÁ¤ 3.»èÁ¦ 4.¸®½ºÆ®");
+					System.out.println("5.ÆòÁ¡ 6.ÀÌ¸§°Ë»ö 7.ÇĞ¹ø°Ë»ö 8.Á¾·á");
+					System.out.print("¼±ÅÃ => ");
 					ch=sc.nextInt();
-				} while(ch<1 || ch>7);
+				} while(ch<1||ch>8);
 				
-				if(ch==7) {
-					return;
+				if(ch==8) {
+					System.exit(0);
 				}
+				
 				switch(ch) {
-				case 1: append(); break;
-				case 2: update(); break;
-				case 3: delete(); break;
-				case 4: findByHak(); break;
-				case 5: findByName(); break;
-				case 6: list(); break;
+				case 1:input(); break;
+				case 2:update(); break;
+				case 3:delete(); break;
+				case 4:printAll(); break;
+				case 5:printGrade(); break;
+				case 6:findByName(); break;
+				case 7:findByHak(); break;
 				}
 			} catch (InputMismatchException e) {
 				sc.nextLine();
 			}
 		}
 	}
-	public void append() {
-		System.out.println("ì¶”ê°€");
-		ScoreVO vo = new ScoreVO();
-		try {
-			System.out.print("í•™ë²ˆ?");
-			vo.setHak(sc.next());
-			System.out.print("ì´ë¦„?");
-			vo.setName(sc.next());
-			System.out.print("êµ­ì–´?");
-			vo.setKor(sc.nextInt());
-			System.out.print("ìˆ˜í•™?");
-			vo.setMat(sc.nextInt());
-			System.out.print("ì˜ì–´?");
-			vo.setEng(sc.nextInt());
-			
-			boolean b = ss.insertScore(vo);
-			if(b)
-				System.out.println("ì¶”ê°€ ì„±ê³µ");
-			else
-				System.out.println("í•™ë²ˆ ì¤‘ë³µìœ¼ë¡œ ì¶”ê°€ ì‹¤íŒ¨");
-			
-		} catch (InputMismatchException e) {
-			System.out.println("ì ìˆ˜ëŠ” ìˆ«ìë§Œ ì…ë ¥ ê°€ëŠ¥");
-			sc.nextLine();
-		}
-		System.out.println();
 	
-	}
-	
-	public void update() {
-		System.out.println("ìˆ˜ì •");
-		ScoreVO vo = new ScoreVO();
+	public void input() {
+		System.out.println("\nÀÚ·á ÀÔ·Â...");
 		
-		System.out.println("ìˆ˜ì •í•  í•™ë²ˆ?");
-		vo=ss.readScore(sc.next());
-		if(vo==null)
-			{System.out.println("ì¡´ì¬í•˜ì§€ ì•ŠëŠ” í•™ë²ˆ");
+		ScoreVO vo=new ScoreVO();
+		try {
+			System.out.print("ÇĞ¹ø?");
+			vo.setHak(sc.next());
+			
+			if(ss.readScore(vo.getHak())!=null) {
+				System.out.println("µî·ÏµÈ ÇĞ¹øÀÔ´Ï´Ù.\n");
+				return;
+			}
+			System.out.print("ÀÌ¸§?");
+			vo.setName(inputName()); 
+			
+			System.out.print("±¹¾î?");
+			vo.setKor(inputScore());
+			
+			System.out.print("¿µ¾î?");
+			vo.setEng(inputScore());
+			
+			System.out.print("¼öÇĞ?");
+			vo.setMat(inputScore());
+			
+			ss.append(vo);
+			
+			System.out.println("µî·Ï¿Ï·á...");
+
+		} catch (InputMismatchException e) {
+			System.out.println("Á¡¼ö´Â ¼ıÀÚ¸¸ °¡´ÉÇÕ´Ï´Ù.");
+		} catch (ScoreValidException e) {
+			System.out.println(e.getMessage());
+		} catch (KoreanValidException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println();
+	}
+
+	public void update() {
+		System.out.println("\nÀÚ·á ¼öÁ¤...");
+		
+		System.out.print("¼öÁ¤ÇÒ ÇĞ¹ø?");
+		String hak=sc.next();
+		
+		ScoreVO vo=ss.readScore(hak);
+		if(vo==null) {
+			System.out.println("Á¸ÀçÇÏÁö ¾Ê´Â ÇĞ¹øÀÔ´Ï´Ù.\n");
 			return;
 		}
 		
-		while(true) {
+		String name;
+		int kor, eng, mat;
+		
 		try {
-			System.out.print("ì´ë¦„?");
-			vo.setName(sc.next());
-			System.out.print("êµ­ì–´?");
-			vo.setKor(sc.nextInt());
-			System.out.print("ìˆ˜í•™?");
-			vo.setMat(sc.nextInt());
-			System.out.print("ì˜ì–´?");
-			vo.setEng(sc.nextInt());
-			System.out.println("ìˆ˜ì • ì™„ë£Œ");
-			break;
+			System.out.print("ÀÌ¸§?");
+			name = inputName();
+			
+			System.out.print("±¹¾î?");
+			kor = inputScore();
+			
+			System.out.print("¿µ¾î?");
+			eng = inputScore();
+
+			System.out.print("¼öÇĞ?");
+			mat = inputScore();
+
+			vo.setName(name);
+			vo.setKor(kor);
+			vo.setEng(eng);
+			vo.setMat(mat);
+			
+			System.out.println("¼öÁ¤ ¿Ï·á...");
+			
 		} catch (InputMismatchException e) {
-			System.out.println("ì ìˆ˜ëŠ” ìˆ«ìë§Œ ì…ë ¥ ê°€ëŠ¥");
-			sc.nextLine();
+			System.out.println("Á¡¼ö´Â ¼ıÀÚ¸¸ °¡´ÉÇÕ´Ï´Ù.");
+		} catch (ScoreValidException e) {
+			System.out.println(e.getMessage());
+		} catch (KoreanValidException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		
 		System.out.println();
-		}
 	}
 	
 	public void delete() {
-		System.out.println("ì‚­ì œ");
+		System.out.println("\nÀÚ·á »èÁ¦...");
 		
 		String hak;
-		System.out.println("ì‚­ì œí•  í•™ë²ˆ?");
+		System.out.print("»èÁ¦ÇÒ ÇĞ¹ø?");
 		hak=sc.next();
 		
-		if(ss.deleteScore(hak)) {
-			System.out.println("ì‚­ì œ ì™„ë£Œ");
-		} else {
-			System.out.println("ì‚­ì œì‹¤íŒ¨");
-		}
-	System.out.println();
-	}
-	
-	public void findByHak(){
-		System.out.println("í•™ë²ˆìœ¼ë¡œ ì°¾ê¸°");
-		
-		String hak;
-		System.out.println("ì°¾ì„ í•™ë²ˆ?");
-		hak=sc.next();
-		
-		if(ss.readScore(hak)==null)
-			{System.out.println("ì¡´ì¬í•˜ì§€ ì•ŠëŠ” í•™ë²ˆ");
-			return;
-			}
-		
-		System.out.println(vo.getHak()+"\t");
-		
-	}
-	
-	public void findByName(){
-		System.out.println("ì´ë¦„ ê²€ìƒ‰");
-	
-		String name;
-		name=sc.next();
-		
-		List<scoreVO> list=ss.listScore(name);
-		if(list.size()==0) {
-			System.out.println("ë“±ë¡ëœ ìë£Œê°€ ì—†ìŠµë‹ˆë‹¤.");
+		if(! ss.remove(hak)) {
+			System.out.println("µî·ÏµÈ ÇĞ¹øÀÌ ¾Æ´Õ´Ï´Ù.\n");
 			return;
 		}
 		
-		for(ScoreVO vo:list) {
-			System.out.println(vo.getHak()+"\t");
+		System.out.println("»èÁ¦¿Ï·á...\n");
+	}
+
+	public void printAll() {
+		System.out.println("\nÀüÃ¼ ¸®½ºÆ®...");
+		
+		ScoreVO[] list = ss.listScore();
+		for(int i=0; i<ss.getCount(); i++) {
+			ScoreVO vo = list[i];
+			System.out.println(vo);
+			// System.out.println(vo.toString());
 		}
 		System.out.println();
+		
 	}
 	
-	
-	public void list() {
-		System.out.println("\nì „ì²´ë¦¬ìŠ¤íŠ¸");
+	public void printGrade() {
+		System.out.println("\nÆòÁ¡ Ãâ·Â...");
 		
-		for(ScoreVO vo : ss.listScore()) {
+		ScoreVO[] list = ss.listScore();
+		for(int i=0; i<ss.getCount(); i++) {
+			ScoreVO vo=list[i];
 			System.out.print(vo.getHak()+"\t");
 			System.out.print(vo.getName()+"\t");
-			System.out.print(vo.getKor()+"\t");
-			System.out.print(vo.getEng()+"\t");
-			System.out.print(vo.getMat()+"\t");
-			System.out.print(vo.getTot()+"\t");
-			System.out.print(vo.getAve()+"\n");
+			System.out.print(ss.grade(vo.getKor())+"\t");
+			System.out.print(ss.grade(vo.getEng())+"\t");
+			System.out.print(ss.grade(vo.getMat())+"\n");
 		}
 		System.out.println();
+	}
+	
+	public void findByName() {
+		System.out.println("\nÀÌ¸§ °Ë»ö...");
+		
+		String name;
+		System.out.print("°Ë»öÇÒ ÀÌ¸§?");
+		name=sc.next();
+		
+		ScoreVO[] list = ss.listScore();
+		for(int i=0; i<ss.getCount(); i++) {
+			ScoreVO vo=list[i];
+			// if(vo.getName().startsWith(name)) {
+			if(vo.getName().indexOf(name)>=0) {
+				System.out.println(vo);
+			}
+		}
+		System.out.println();
+	}
+	
+	public void findByHak() {
+		System.out.println("\nÇĞ¹ø °Ë»ö...");
+		
+		String hak;
+		System.out.print("°Ë»öÇÒ ÇĞ¹ø?");
+		hak=sc.next();
+		
+		ScoreVO vo=ss.readScore(hak);
+		if(vo==null) {
+			System.out.println("µî·ÏµÈ ÇĞ¹øÀÌ ¾Æ´Õ´Ï´Ù.\n");
+			return;
+		}
+		
+		System.out.println(vo+"\n");
+	}
+	
+	private String inputName() throws KoreanValidException {
+		String s=null;
+		String regex="^[°¡-ÆR]+$";
+		s=sc.next();
+		
+		if(! s.matches(regex)) {
+			throw new KoreanValidException("ÀÌ¸§Àº ÇÑ±Û¸¸ °¡´ÉÇÕ´Ï´Ù.");
+		}
+		
+		return s;
+	}
+	
+	private int inputScore() throws ScoreValidException {
+		int s=0;
+		
+		try {
+			s=sc.nextInt();
+			if(s<0 || s>100)
+				throw new ScoreValidException("Á¡¼ö´Â 0~100 »çÀÌ¸¸ °¡´ÉÇÕ´Ï´Ù.");
+		} catch (InputMismatchException e) {
+			sc.next();
+			throw e;
+		}
+		
+		return s;
 	}
 }
